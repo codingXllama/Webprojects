483b9ec89ccdb58d65586768623840218441b389
codingxllama <osamaalchalabi2@gmail.com>
2022-06-25 23:55:53 -0400

    exo1

---
+++ Responsive website/ex01.js
@@ -0,0 +1,15 @@
+var today = new Date();
+
+var day = today.getDate();
+
+var daylist = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+var hour = today.getHours();
