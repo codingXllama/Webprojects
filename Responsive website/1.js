85d5aca7a6adcd63984fec4d2315d794b373b61f
codingxllama <osamaalchalabi2@gmail.com>
2022-07-22 23:42:18 -0400

    main file

--- Responsive website/1.js
+++ Responsive website/1.js
@@ -1,35 +1,31 @@
 var today = new Date();
-
-var day = today.getDate();
-
+var day = today.getDay();
 var daylist = [
   "Sunday",
   "Monday",
   "Tuesday",
-  "Wednesday",
+  "Wednesday ",
   "Thursday",
   "Friday",
   "Saturday",
 ];
-
+console.log("Today is : " + daylist[day] + ".");
 var hour = today.getHours();
 var minute = today.getMinutes();
-var seconds = today.getSeconds();
-
-var prepand = hour >= 12 ? hour : hour;
-
+var second = today.getSeconds();
+var prepand = hour >= 12 ? " PM " : " AM ";
+hour = hour >= 12 ? hour - 12 : hour;
 if (hour === 0 && prepand === " PM ") {
   if (minute === 0 && second === 0) {
     hour = 12;
-    prepand = "Noon";
+    prepand = " Noon";
   } else {
     hour = 12;
     prepand = " PM";
   }
 }
-
-if (hour === 0 && prepand === " AM") {
-  if (minute === 0 && seconds === 0) {
+if (hour === 0 && prepand === " AM ") {
+  if (minute === 0 && second === 0) {
     hour = 12;
     prepand = " Midnight";
   } else {
@@ -37,7 +33,6 @@ if (hour === 0 && prepand === " AM") {
     prepand = " AM";
   }
 }
-
 console.log(
-  "Current Time: " + hour + prepand + " : " + minute + " : " + seconds
+  "Current Time : " + hour + prepand + " : " + minute + " : " + second
 );
