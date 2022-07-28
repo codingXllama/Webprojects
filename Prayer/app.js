61cf83fec92a5eb36e09bb6d39f31a92656c9c1b
codingxllama <osamaalchalabi2@gmail.com>
2022-07-28 00:18:14 -0400

    updated the time for every second

--- Prayer/app.js
+++ Prayer/app.js
@@ -6,6 +6,56 @@ var currentDate = new Date().toLocaleDateString("en-US");
 // var x = currentDate.toISOString().slice(0, 10);
 // var enDate = x.toLocaleDateString("en-us");
 
-currentTimeID.textContent = currentTime;
+// currentTimeID.textContent = currentTime;
 currentDate_ID.textContent = currentDate.slice(0, 10);
-setInterval(currentTime, 1000);
+// setInterval(currentTime, 1000);
+
+function doDate() {
+  var str = "";
+  var days = new Array(
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  );
+
+  var months = new Array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  );
+
+  var now = new Date();
+
+  str +=
+    "Today is: " +
+    days[now.getDay()] +
+    ", " +
+    now.getDate() +
+    " " +
+    months[now.getMonth()] +
+    "" +
+    now.getFullYear() +
+    " " +
+    now.getHours() +
+    ":" +
+    now.getMinutes() +
+    ":" +
+    now.getSeconds();
+
+  var currentTime = new Date().toLocaleTimeString();
+  document.getElementById("currentTime").innerHTML = currentTime;
+}
+setInterval(doDate, 1000);
